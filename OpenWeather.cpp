4a18529d83538a98a1d784cfa0940bfad9b78d12
Matt-prog <55807356+Matt-prog@users.noreply.github.com>
2019-11-17 22:19:47 +0100

    Add files via upload

---
+++ OpenWeather.cpp
@@ -0,0 +1,178 @@
+#include "OpenWeather.h"
+
+#ifdef ESP8266 //code only for ESP8266
+
+/*OpenWeather::OpenWeather(String key_){
+  API_key = key_;
+}*/
+
+void OPEN_WEATHER::key(String key_){
+  API_key = key_;
+}
+
+byte OPEN_WEATHER::getWeather(Weather *ret,String dest, byte units, char* language){
+  HTTPClient http;
+  
+  String openweatherURL = "http://api.openweathermap.org/data/2.5/weather?";
+  openweatherURL += dest;
+  if(units == metric) openweatherURL += "&units=metric";
+  else if(units == imperial) openweatherURL += "&units=imperial";
+  if(language[0] != 0) openweatherURL += "&lang=" + String(language);
+  openweatherURL += "&appid=";
+  openweatherURL += API_key;
+  
+  http.begin(openweatherURL);
+  
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    String payload = http.getString();
+    //Serial.println(payload);
+    JsonSerialize(payload,ret);
+    ret->sunrise += ret->timezone;
+    //ret->sunrise %= 86400L;
+    ret->sunset += ret->timezone;
+    //ret->sunset %= 86400L;
+    ret->data_time += ret->timezone;
+    ret->status = httpCode;
+    ret->units = units;
+  }
+  http.end();   //Close connection
+  return ret->status;
+}
+
+Weather OPEN_WEATHER::getWeatherByCityName(String dest, String country_code, byte units, char* language){
+  Weather ret;
+  dest = "&q="+dest;
+  if(country_code[0] != 0) dest += "," + String(country_code);
+  dest.replace(" ", "+"); //HTML encoding does not allow spaces
+  getWeather(&ret,dest,units,language);
+  return ret;
+}
+
+Weather OPEN_WEATHER::getWeatherByCityName(String dest, byte units, char* language){
+  Weather ret;
+  dest = "&q="+dest;
+  dest.replace(" ", "+"); //HTML encoding does not allow spaces
+  getWeather(&ret,dest,units,language);
+  return ret;
+}
+
+Weather OPEN_WEATHER::getWeatherByID(unsigned long ID, byte units, char* language){
+  Weather ret;
+  getWeather(&ret,"&id="+String(ID),units,language);
+  return ret;
+}
+
+Weather OPEN_WEATHER::getWeatherByZIP(unsigned long ZIP, String country_code, byte units, char* language){
+  String destination = "&zip="+String(ZIP);
+  if(country_code[0] != 0) destination += "," + String(country_code);
+  Weather ret;
+  getWeather(&ret,destination,units,language);
+  return ret;
+}
+
+Weather OPEN_WEATHER::getWeatherByZIP(unsigned long ZIP, byte units, char* language){
+  String destination = "&zip="+String(ZIP);
+  Weather ret;
+  getWeather(&ret,destination,units,language);
+  return ret;
+}
+
+Weather OPEN_WEATHER::getWeatherByCoords(float latitude, float longitude, byte units, char* language){
+  Weather ret;
+  getWeather(&ret,"&lat="+String(latitude) + "&lon=" + String(longitude),units,language);
+  return ret;
+}
+
+void OPEN_WEATHER::JsonSerialize(String json, Weather *I){
+  int json_length = json.length();
+  byte object = 0;
+  bool key_started = false;
+  bool key_end = false;
+  bool value_started = false;
+  bool com_en = false;
+  String key = "";
+  String value = "";
+  for(int i = 0; i < json_length; i++){
+    if(json.charAt(i) == '"' || (key_started && key_end && !value_started && json.charAt(i) == ':' && json.charAt(i+1) >= '0' && json.charAt(i+1) <= '9') || (key_started && key_end && value_started && !com_en && (json.charAt(i) == ',' || json.charAt(i) == '}'))){
+      if(value_started){ //end of value found
+        key_started = false;
+        key_end = false;
+        value_started = false;
+          if(key == "lon" && object == 1) I->longitude = value.toFloat();
+          else if(key == "lat" && object == 1) I->latitude = value.toFloat();
+          else if(key == "id" && object == 2) I->weather_id = value.toInt();
+          else if(key == "main" && object == 2) strcpy(I->weather_main, value.c_str());
+          else if(key == "description" && object == 2) strcpy(I->weather_description, value.c_str());
+          else if(key == "icon" && object == 2) strcpy(I->weather_icon, value.c_str());
+          else if(key == "temp" && object == 4) I->temp = value.toFloat();
+          else if(key == "pressure" && object == 4) I->pressure = value.toFloat();
+          else if(key == "humidity" && object == 4) I->humidity = value.toFloat();
+          else if(key == "temp_min" && object == 4) I->temp_min = value.toFloat();
+          else if(key == "temp_max" && object == 4) I->temp_max = value.toFloat();
+          else if(key == "visibility" && object == 0) I->visibility = value.toInt();
+          else if(key == "speed" && object == 5) I->wind_speed = value.toFloat();
+          else if(key == "deg" && object == 5) I->wind_direction = value.toInt();
+          else if(key == "all" && object == 6) I->clouds = value.toInt();
+          else if(key == "1h" && object == 7) I->rain_1h = value.toFloat();
+          else if(key == "3h" && object == 7) I->rain_3h = value.toFloat();
+          else if(key == "1h" && object == 8) I->snow_1h = value.toFloat();
+          else if(key == "3h" && object == 8) I->snow_3h = value.toFloat();
+          else if(key == "country" && object == 9) strcpy(I->country, value.c_str());
+          else if(key == "sunrise" && object == 9) I->sunrise = atol(value.c_str());
+          else if(key == "sunset" && object == 9) I->sunset = atol(value.c_str());
+          else if(key == "timezone" && object == 0) I->timezone = value.toInt();
+          else if(key == "id" && object == 0) I->city_id = value.toInt();
+          else if(key == "name" && object == 0) strcpy(I->city_name, value.c_str());
+          else if(key == "dt" && object == 0) I->data_time = atol(value.c_str());
+
+        if(json.charAt(i) == '}'){
+          if(object == 0){
+            break; //end of json
+          }
+          object = 0; //end of object
+        }
+      }
+      else if(key_end){
+        value_started = true;
+        value = "";
+        com_en = json.charAt(i) == '"';
+      }
+      else if(key_started) key_end = true;
+      else{
+        key_started = true;
+        key = "";
+      }
+    }
+    else if(json.charAt(i) == '{' && key_end && !value_started){ //new object found
+      if(key == "coord") object = 1;
+      else if(key == "weather") object = 2;
+      else if(key == "base") object = 3;
+      else if(key == "main") object = 4;
+      else if(key == "wind") object = 5;
+      else if(key == "clouds") object = 6;
+      else if(key == "rain") object = 7;
+      else if(key == "snow") object = 8;
+      else if(key == "sys") object = 9;
+      else object = 10;
+      key_end = false;
+      key_started = false;
+    }
+    else if(json.charAt(i) == '}'){
+      if(object == 0){
+        break; //end of json
+      }
+      object = 0; //end of object
+    }
+    else if(key_started && !key_end){
+      key += json.charAt(i);
+    }
+    else if(key_started && key_end && value_started){
+      value += json.charAt(i);
+    }
+  }
+}
+
+OPEN_WEATHER OpenWeather;
+
+#endif
---
+++ OpenWeather.h
@@ -0,0 +1,91 @@
+#ifdef ESP8266 //code only for ESP8266
+
+#ifndef OpenWeather_h
+#define OpenWeather_h
+
+#if (ARDUINO >= 100)
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+
+/*! CPP guard */
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+typedef struct{
+  float longitude = 0.00;
+  float latitude = 0.00;
+  int weather_id = 0;                 //Weather condition ID
+  char weather_main[40] = "";         //Weather main text (Rain, Clear,...)
+  char weather_description[40] = "";  //Weather description
+  char weather_icon[10] = "";         //Weather icon (to get weather icon use URL: "http://openweathermap.org/img/wn/" + weather_icon + "@2x.png")
+  float temp = 0.00;                  //Actual temperature (Units: Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit)
+  float temp_max = 0.00;              //Actual maximum temperature (Units: Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit)
+  float temp_min = 0.00;              //Actual minimum temperature (Units: Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit)
+  float pressure = 0.00;              //Actual pressure in hPa
+  float humidity = 0.00;              //Actual humidity in %
+  int visibility = 0;                 //Visibility in meters
+  float wind_speed = 0.00;            //Actual wind speed (Units: Default: meter/sec, Metric: meter/sec, Imperial: miles/hour)
+  short wind_direction = 0;           //Wind direction in degrees
+  byte clouds = 0;                    //Cloudiness in %
+  float rain_1h = 0.00;               //Rain volume for the last 1 hour in mm
+  float rain_3h = 0.00;               //Rain volume for the last 3 hours in mm
+  float snow_1h = 0.00;               //Snow volume for the last 1 hour in mm
+  float snow_3h = 0.00;               //Snow volume for the last 3 hours in mm
+  unsigned long sunrise = 0;          //Sunrise in seconds UNIX (including timezone)
+  unsigned long sunset = 0;           //Sunset in seconds UNIX (including timezone)
+  char country[6] = "";               //Country code (GB, USA,...)
+  long timezone = 0;                  //Timezone offset in seconds
+  char city_name[60] = "";            //City name (London, ...)
+  unsigned long city_id = 0;          //City ID
+  byte units = 0;                     //Units of response
+  unsigned long data_time = 0;        //Data receiving time in seconds UNIX (including timezone)
+  int status = 0;                     //Status of received message (> 0 HTTP codes, -1 to -11 are HTTP client errors defined in ESP8266HTTPClient.h,
+                                      // 404 is CITY_NOT_FOUND
+                                      // 401 invalid API key
+} Weather;
+
+//Units enums
+enum{
+  default_, //Kelvin, meters per seconds
+  metric,   //Celsius, meters per seconds
+  imperial  //Fahrenheit, miles per seconds
+};
+//Change will appear only in values: temp, temp_max, temp_min, wind_speed
+
+class OPEN_WEATHER{
+  public:
+  //OpenWeather(String key_);
+  void key(String key_);
+  byte getWeather(Weather *ret,String dest, byte units = default_, char* language = "");
+  Weather getWeatherByCityName(String dest, String country_code = "", byte units = default_, char* language = "");
+  Weather getWeatherByCityName(String dest, byte units = default_, char* language = "");
+  Weather getWeatherByID(unsigned long ID, byte units = default_, char* language = "");
+  Weather getWeatherByZIP(unsigned long ZIP, String country_code = "", byte units = default_, char* language = "");
+  Weather getWeatherByZIP(unsigned long ZIP, byte units = default_, char* language = "");
+  Weather getWeatherByCoords(float latitude, float longitude, byte units = default_, char* language = "");
+
+  private:
+  void JsonSerialize(String json, Weather *I);
+  String API_key;
+};
+
+extern OPEN_WEATHER OpenWeather;
+
+
+/*! CPP guard */
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+#endif
