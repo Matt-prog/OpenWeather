b1e46d76d2e4ecd378be92691c4d7b6c24c5fad2
Matt-prog <55807356+Matt-prog@users.noreply.github.com>
2019-11-18 22:25:30 +0100

    Add files via upload

--- OpenWeather.cpp
+++ OpenWeather.cpp
@@ -13,12 +13,12 @@ void OPEN_WEATHER::key(String key_){
 byte OPEN_WEATHER::getWeather(Weather *ret,String dest, byte units, char* language){
   HTTPClient http;
   
-  String openweatherURL = "http://api.openweathermap.org/data/2.5/weather?";
+  String openweatherURL = String(F("http://api.openweathermap.org/data/2.5/weather?"));
   openweatherURL += dest;
-  if(units == metric) openweatherURL += "&units=metric";
-  else if(units == imperial) openweatherURL += "&units=imperial";
-  if(language[0] != 0) openweatherURL += "&lang=" + String(language);
-  openweatherURL += "&appid=";
+  if(units == metric) openweatherURL += String(F("&units=metric"));
+  else if(units == imperial) openweatherURL += String(F("&units=imperial"));
+  if(language[0] != 0) openweatherURL += String(F("&lang=")) + String(language);
+  openweatherURL += String(F("&appid="));
   openweatherURL += API_key;
   
   http.begin(openweatherURL);
@@ -42,7 +42,7 @@ byte OPEN_WEATHER::getWeather(Weather *ret,String dest, byte units, char* langua
 
 Weather OPEN_WEATHER::getWeatherByCityName(String dest, String country_code, byte units, char* language){
   Weather ret;
-  dest = "&q="+dest;
+  dest = String(F("&q="))+dest;
   if(country_code[0] != 0) dest += "," + String(country_code);
   dest.replace(" ", "+"); //HTML encoding does not allow spaces
   getWeather(&ret,dest,units,language);
@@ -51,7 +51,7 @@ Weather OPEN_WEATHER::getWeatherByCityName(String dest, String country_code, byt
 
 Weather OPEN_WEATHER::getWeatherByCityName(String dest, byte units, char* language){
   Weather ret;
-  dest = "&q="+dest;
+  dest = String(F("&q="))+dest;
   dest.replace(" ", "+"); //HTML encoding does not allow spaces
   getWeather(&ret,dest,units,language);
   return ret;
@@ -59,12 +59,12 @@ Weather OPEN_WEATHER::getWeatherByCityName(String dest, byte units, char* langua
 
 Weather OPEN_WEATHER::getWeatherByID(unsigned long ID, byte units, char* language){
   Weather ret;
-  getWeather(&ret,"&id="+String(ID),units,language);
+  getWeather(&ret,String(F("&id="))+String(ID),units,language);
   return ret;
 }
 
 Weather OPEN_WEATHER::getWeatherByZIP(unsigned long ZIP, String country_code, byte units, char* language){
-  String destination = "&zip="+String(ZIP);
+  String destination = String(F("&zip="))+String(ZIP);
   if(country_code[0] != 0) destination += "," + String(country_code);
   Weather ret;
   getWeather(&ret,destination,units,language);
@@ -72,7 +72,7 @@ Weather OPEN_WEATHER::getWeatherByZIP(unsigned long ZIP, String country_code, by
 }
 
 Weather OPEN_WEATHER::getWeatherByZIP(unsigned long ZIP, byte units, char* language){
-  String destination = "&zip="+String(ZIP);
+  String destination = String(F("&zip="))+String(ZIP);
   Weather ret;
   getWeather(&ret,destination,units,language);
   return ret;
@@ -80,7 +80,7 @@ Weather OPEN_WEATHER::getWeatherByZIP(unsigned long ZIP, byte units, char* langu
 
 Weather OPEN_WEATHER::getWeatherByCoords(float latitude, float longitude, byte units, char* language){
   Weather ret;
-  getWeather(&ret,"&lat="+String(latitude) + "&lon=" + String(longitude),units,language);
+  getWeather(&ret,String(F("&lat="))+String(latitude) + String(F("&lon=")) + String(longitude),units,language);
   return ret;
 }
 
@@ -99,32 +99,32 @@ void OPEN_WEATHER::JsonSerialize(String json, Weather *I){
         key_started = false;
         key_end = false;
         value_started = false;
-          if(key == "lon" && object == 1) I->longitude = value.toFloat();
-          else if(key == "lat" && object == 1) I->latitude = value.toFloat();
-          else if(key == "id" && object == 2) I->weather_id = value.toInt();
-          else if(key == "main" && object == 2) strcpy(I->weather_main, value.c_str());
-          else if(key == "description" && object == 2) strcpy(I->weather_description, value.c_str());
-          else if(key == "icon" && object == 2) strcpy(I->weather_icon, value.c_str());
-          else if(key == "temp" && object == 4) I->temp = value.toFloat();
-          else if(key == "pressure" && object == 4) I->pressure = value.toFloat();
-          else if(key == "humidity" && object == 4) I->humidity = value.toFloat();
-          else if(key == "temp_min" && object == 4) I->temp_min = value.toFloat();
-          else if(key == "temp_max" && object == 4) I->temp_max = value.toFloat();
-          else if(key == "visibility" && object == 0) I->visibility = value.toInt();
-          else if(key == "speed" && object == 5) I->wind_speed = value.toFloat();
-          else if(key == "deg" && object == 5) I->wind_direction = value.toInt();
-          else if(key == "all" && object == 6) I->clouds = value.toInt();
-          else if(key == "1h" && object == 7) I->rain_1h = value.toFloat();
-          else if(key == "3h" && object == 7) I->rain_3h = value.toFloat();
-          else if(key == "1h" && object == 8) I->snow_1h = value.toFloat();
-          else if(key == "3h" && object == 8) I->snow_3h = value.toFloat();
-          else if(key == "country" && object == 9) strcpy(I->country, value.c_str());
-          else if(key == "sunrise" && object == 9) I->sunrise = atol(value.c_str());
-          else if(key == "sunset" && object == 9) I->sunset = atol(value.c_str());
-          else if(key == "timezone" && object == 0) I->timezone = value.toInt();
-          else if(key == "id" && object == 0) I->city_id = value.toInt();
-          else if(key == "name" && object == 0) strcpy(I->city_name, value.c_str());
-          else if(key == "dt" && object == 0) I->data_time = atol(value.c_str());
+          if(key == F("lon") && object == 1) I->longitude = value.toFloat();
+          else if(key == F("lat") && object == 1) I->latitude = value.toFloat();
+          else if(key == F("id") && object == 2) I->weather_id = value.toInt();
+          else if(key == F("main") && object == 2) strcpy(I->weather_main, value.c_str());
+          else if(key == F("description") && object == 2) strcpy(I->weather_description, value.c_str());
+          else if(key == F("icon") && object == 2) strcpy(I->weather_icon, value.c_str());
+          else if(key == F("temp") && object == 4) I->temp = value.toFloat();
+          else if(key == F("pressure") && object == 4) I->pressure = value.toFloat();
+          else if(key == F("humidity") && object == 4) I->humidity = value.toFloat();
+          else if(key == F("temp_min") && object == 4) I->temp_min = value.toFloat();
+          else if(key == F("temp_max") && object == 4) I->temp_max = value.toFloat();
+          else if(key == F("visibility") && object == 0) I->visibility = value.toInt();
+          else if(key == F("speed") && object == 5) I->wind_speed = value.toFloat();
+          else if(key == F("deg") && object == 5) I->wind_direction = value.toInt();
+          else if(key == F("all") && object == 6) I->clouds = value.toInt();
+          else if(key == F("1h") && object == 7) I->rain_1h = value.toFloat();
+          else if(key == F("3h") && object == 7) I->rain_3h = value.toFloat();
+          else if(key == F("1h") && object == 8) I->snow_1h = value.toFloat();
+          else if(key == F("3h") && object == 8) I->snow_3h = value.toFloat();
+          else if(key == F("country") && object == 9) strcpy(I->country, value.c_str());
+          else if(key == F("sunrise") && object == 9) I->sunrise = atol(value.c_str());
+          else if(key == F("sunset") && object == 9) I->sunset = atol(value.c_str());
+          else if(key == F("timezone") && object == 0) I->timezone = value.toInt();
+          else if(key == F("id") && object == 0) I->city_id = value.toInt();
+          else if(key == F("name") && object == 0) strcpy(I->city_name, value.c_str());
+          else if(key == F("dt") && object == 0) I->data_time = atol(value.c_str());
 
         if(json.charAt(i) == '}'){
           if(object == 0){
@@ -145,15 +145,15 @@ void OPEN_WEATHER::JsonSerialize(String json, Weather *I){
       }
     }
     else if(json.charAt(i) == '{' && key_end && !value_started){ //new object found
-      if(key == "coord") object = 1;
-      else if(key == "weather") object = 2;
-      else if(key == "base") object = 3;
-      else if(key == "main") object = 4;
-      else if(key == "wind") object = 5;
-      else if(key == "clouds") object = 6;
-      else if(key == "rain") object = 7;
-      else if(key == "snow") object = 8;
-      else if(key == "sys") object = 9;
+      if(key == F("coord")) object = 1;
+      else if(key == F("weather")) object = 2;
+      else if(key == F("base")) object = 3;
+      else if(key == F("main")) object = 4;
+      else if(key == F("wind")) object = 5;
+      else if(key == F("clouds")) object = 6;
+      else if(key == F("rain")) object = 7;
+      else if(key == F("snow")) object = 8;
+      else if(key == F("sys")) object = 9;
       else object = 10;
       key_end = false;
       key_started = false;
